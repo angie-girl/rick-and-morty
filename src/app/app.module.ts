7fa8d0e76887b82e4b00f55ed4c3fa1d6633acba
Angelika <angelika@sonic.net.pl>
2021-10-15 22:34:38 +0200

    Create components
    - Created 3 components: character, location and episode
    - Created global classes with margin-top and margin-bottom

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgxPaginationModule } from "ngx-pagination";
 import { NavbarComponent } from './_shared/navbar/navbar.component';
 import { FooterComponent } from './_shared/footer/footer.component';
 import { MainComponent } from './_pages/main/main.component';
+import { CharacterComponent } from './_pages/character/character.component';
+import { LocationComponent } from './_pages/location/location.component';
+import { EpisodeComponent } from './_pages/episode/episode.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FooterComponent,
-    MainComponent
+    MainComponent,
+    CharacterComponent,
+    LocationComponent,
+    EpisodeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    NgxPaginationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
