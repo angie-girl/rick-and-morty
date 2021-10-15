f043727075ec5ed39e2d1d8dea656197299089f7
Angelika <angelika@sonic.net.pl>
2021-10-15 22:44:08 +0200

    Add routing
    - Added path to each component
    - Created function for navigating to the home page
    - Changed href to routerLink with correct path
    - Created functions for navigating to the pages with tables (character, location and episode) and implemented in html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CharacterComponent } from './_pages/character/character.component';
+import { EpisodeComponent } from './_pages/episode/episode.component';
+import { LocationComponent } from './_pages/location/location.component';
 import { MainComponent } from './_pages/main/main.component';
 
 const routes: Routes = [
-  {path: '', component: MainComponent}
+  {path: '', component: MainComponent},
+  {path: 'character', component: CharacterComponent},
+  {path: 'location', component: LocationComponent},
+  {path: 'episode', component: EpisodeComponent}
 ];
 
 @NgModule({
