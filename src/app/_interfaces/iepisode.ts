b3f1c949f8801092dea99a8edfcd9799f438996f
Angelika <angelika@sonic.net.pl>
2021-10-17 21:12:17 +0200

    Implement API with all episodes
    - Created interface
    - Created episode service and implemented API
    - Added content - created table
    - Added pagination

---
+++ src/app/_interfaces/iepisode.ts
@@ -0,0 +1,9 @@
+export interface IEpisode {
+    id: number;
+    name: string;
+    air_data: string;
+    episode: string;
+    characters: [];
+    url: string;
+    created: string;
+}
--- src/app/_pages/episode/episode.component.ts
+++ src/app/_pages/episode/episode.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IEpisode } from 'src/app/_interfaces/iepisode';
+import { EpisodeService } from 'src/app/_services/episode.service';
 
 @Component({
   selector: 'app-episode',
@@ -6,10 +9,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./episode.component.scss']
 })
 export class EpisodeComponent implements OnInit {
+  public episodes: IEpisode[] = [];
+  maxSize = 10;
+  public perPage = 20;
+  p: number = 1;
+  public totalItems = 0;
 
-  constructor() { }
+  constructor(private episodeService: EpisodeService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.getEpisodes(this.p);
   }
 
+  getEpisodes(page: number): void {
+    this.p = page
+
+    let params = {
+      count: this.perPage,
+      page: this.p
+    }
+
+    this.route.queryParams.subscribe(q => {
+      params = {...params, ...q}
+    });
+    this.episodeService.getEpisodes(params).subscribe(res => {
+      this.episodes = res.results;
+      this.totalItems = res.info.count;
+    })
+  }
 }
