06d439b4beeab653bdbb4bd80f6f71a878331721
Angelika <angelika@sonic.net.pl>
2021-10-17 21:10:43 +0200

    Implement API with all locations
    - Added API link in environments
    - Created interface
    - Created location service and implemented API
    - Added content - created table
    - Added pagination

--- src/app/_pages/location/location.component.ts
+++ src/app/_pages/location/location.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ILocation } from 'src/app/_interfaces/ilocation';
+import { LocationService } from 'src/app/_services/location.service';
 
 @Component({
   selector: 'app-location',
@@ -6,10 +9,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./location.component.scss']
 })
 export class LocationComponent implements OnInit {
+  public locations: ILocation[] = [];
+  maxSize = 10;
+  public perPage = 20;
+  p: number = 1;
+  public totalItems = 0;
 
-  constructor() { }
+  constructor(private locationService: LocationService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.getLocations(this.p);
   }
 
+  getLocations(page: number): void {
+    this.p = page
+
+    let params = {
+      count: this.perPage,
+      page: this.p
+    }
+
+    this.route.queryParams.subscribe(q => {
+      params = {...params, ...q}
+    });
+    this.locationService.getLocations(params).subscribe(res => {
+      this.locations = res.results;
+      this.totalItems = res.info.count;
+    })
+  }
 }
